d1ea0c1bab65e26372751c77ac4bd68a629304b4
armin.reiter@live.at <armin.reiter@live.at>
2016-07-07 17:35:01 +0200

    add azure code analyzer and fix warnings

--- CodeHollow.Samples.AzureActiveDirectoryGroups/CodeHollow.Samples.AzureActiveDirectoryGroups/Configuration/AppConfigConfiguration.cs
+++ CodeHollow.Samples.AzureActiveDirectoryGroups/CodeHollow.Samples.AzureActiveDirectoryGroups/Configuration/AppConfigConfiguration.cs
@@ -4,15 +4,17 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.Azure;
 
 namespace CodeHollow.Samples.AzureActiveDirectoryGroups.Configuration
 {
     public class AppConfigConfiguration : IClientConfiguration
     {
-        public string TenantName { get { return ConfigurationManager.AppSettings["TenantName"]; } }
-        public string TenantId { get { return ConfigurationManager.AppSettings["TenantId"]; } }
-        public string ClientId { get { return ConfigurationManager.AppSettings["ClientId"]; } }
-        public string ClientSecret { get { return ConfigurationManager.AppSettings["ClientSecret"]; } }
+        public string TenantName { get { return CloudConfigurationManager.GetSetting("TenantName"); } }
+        public string TenantId { get { return CloudConfigurationManager.GetSetting("TenantId"); } }
+        public string ClientId { get { return CloudConfigurationManager.GetSetting("ClientId"); } }
+        public string ClientSecret { get { return CloudConfigurationManager.GetSetting("ClientSecret"); } }
         public string ClientIdForUserAuthn { get { return ""; } }
         public string AuthString { get { return "https://login.microsoftonline.com/" + TenantName; } }
         public string ResourceUrl { get { return "https://graph.windows.net"; } }
